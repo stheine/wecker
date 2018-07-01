cf65b50b53f284727d16c922301f4db364d57e75
Stefan Heine <stheine@arcor.de>
2018-07-01 21:01:22 +0200

    Add fonts. Add Menu selection display via rotary button.

--- app.js
+++ app.js
@@ -3,13 +3,47 @@
 'use strict';
 
 /* eslint-disable no-console */
+/* eslint-disable camelcase */
+/* eslint-disable no-sync */
 
+const child_process = require('child_process');
+
+const _             = require('lodash');
+const check         = require('check-types');
 // const delay      = require('delay');
 // const font       = require('oled-font-5x7');
 const InputEvent = require('input-event');
 const Oled       = require('../sh1106-js'); // TODO npm module
 
+const glob       = require('./lib/glob');
 const loop       = require('./lib/loop');
+
+
+// Detect the event inputs
+
+const lsinputOut   = String(child_process.execFileSync('/usr/bin/lsinput')); // sudo apt install input-utils
+const lsinputArray = lsinputOut.split(/\n/);
+const devices      = {};
+let   event;
+
+for(const line of lsinputArray) {
+  if(/^\/dev\/input\//.test(line)) {
+    event = line;
+    check.assert.undefined(devices[event], `duplicate event ${event} in ${lsinputOut}`);
+    devices[event] = {};
+  } else if(line === '') {
+    event = null;
+  } else {
+    check.assert.assigned(event, `event not assigned in ${lsinputOut}`);
+
+    let [field, value] = line.split(/:/);
+
+    field = _.trim(field);
+    value = _.trim(value);
+
+    devices[event][field] = value;
+  }
+}
 
 (async() => {
   // Catch CTRL-C
@@ -25,51 +59,49 @@ const loop       = require('./lib/loop');
 
   // Oled
   const oled = new Oled();
-  let   contrast = 0x00;
 
   await oled.initialize();
-  await oled.dimDisplay(contrast);
+  await oled.dimDisplay(0x00);
+//  await oled.clearDisplay(true);
 
 
 
   // Rotary Button
-  const rotaryButtonInput = new InputEvent('/dev/input/event1');
+  const rotaryButtonEvent = _.findKey(devices, {name: '"15.button"'});
+  const rotaryButtonInput = new InputEvent(rotaryButtonEvent);
   const rotaryButton      = new InputEvent.Keyboard(rotaryButtonInput);
 
   rotaryButton.on('keypress', async(/* data */) => {
-    console.log('Button press');
-//    console.log('keypress', data);
+//    console.log('Button press');
 
-    await oled.turnOffDisplay();
+    glob.press();
   });
   rotaryButton.on('keyup', async(/* data */) => {
-    console.log('Button up');
-//    console.log('keyup', data);
+//    console.log('Button up');
 
-    await oled.turnOnDisplay();
+    glob.up();
   });
 
 
 
   // Rotary Encoder
-  const rotaryEncoderInput = new InputEvent('/dev/input/event0');
+  const rotaryEncoderEvent = _.findKey(devices, {name: '"10.rotary"'});
+  const rotaryEncoderInput = new InputEvent(rotaryEncoderEvent);
   const rotaryEncoder      = new InputEvent.Rotary(rotaryEncoderInput);
 
   rotaryEncoder.on('left', async(/* data */) => {
-    console.log('Button left');
-//    console.log('left', data);
+//    console.log('Button left');
 
-    await oled.dimDisplay(--contrast);
-    console.log(`contrast ${contrast}`);
+    glob.left();
   });
   rotaryEncoder.on('right', async(/* data */) => {
-    console.log('Button left');
-//    console.log('right', data);
+//    console.log('Button left');
 
-    await oled.dimDisplay(++contrast);
-    console.log(`contrast ${contrast}`);
+    glob.right();
   });
 
 
+
+  // Startup main loop
   await loop({oled});
 })();
