8fe35be28f9f52d668d62577dad7f78cf147d91a
Stefan Heine <stheine@arcor.de>
2018-07-02 06:58:15 +0200

    code refactoring

--- app.js
+++ app.js
@@ -6,44 +6,11 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-sync */
 
-const child_process = require('child_process');
+const Oled  = require('../sh1106-js'); // TODO npm module
 
-const _             = require('lodash');
-const check         = require('check-types');
-// const delay      = require('delay');
-// const font       = require('oled-font-5x7');
-const InputEvent = require('input-event');
-const Oled       = require('../sh1106-js'); // TODO npm module
-
-const glob       = require('./lib/glob');
-const loop       = require('./lib/loop');
-
-
-// Detect the event inputs
-
-const lsinputOut   = String(child_process.execFileSync('/usr/bin/lsinput')); // sudo apt install input-utils
-const lsinputArray = lsinputOut.split(/\n/);
-const devices      = {};
-let   event;
-
-for(const line of lsinputArray) {
-  if(/^\/dev\/input\//.test(line)) {
-    event = line;
-    check.assert.undefined(devices[event], `duplicate event ${event} in ${lsinputOut}`);
-    devices[event] = {};
-  } else if(line === '') {
-    event = null;
-  } else {
-    check.assert.assigned(event, `event not assigned in ${lsinputOut}`);
-
-    let [field, value] = line.split(/:/);
-
-    field = _.trim(field);
-    value = _.trim(value);
-
-    devices[event][field] = value;
-  }
-}
+const input  = require('./lib/input');
+const loop   = require('./lib/loop');
+const render = require('./lib/render');
 
 (async() => {
   // Catch CTRL-C
@@ -65,42 +32,13 @@ for(const line of lsinputArray) {
 //  await oled.clearDisplay(true);
 
 
-
-  // Rotary Button
-  const rotaryButtonEvent = _.findKey(devices, {name: '"15.button"'});
-  const rotaryButtonInput = new InputEvent(rotaryButtonEvent);
-  const rotaryButton      = new InputEvent.Keyboard(rotaryButtonInput);
-
-  rotaryButton.on('keypress', async(/* data */) => {
-//    console.log('Button press');
-
-    glob.press();
+  // Register the handlers for the input events
+  await input.initialize({
+    press: render.press,
+    up:    render.up,
+    left:  render.left,
+    right: render.right,
   });
-  rotaryButton.on('keyup', async(/* data */) => {
-//    console.log('Button up');
-
-    glob.up();
-  });
-
-
-
-  // Rotary Encoder
-  const rotaryEncoderEvent = _.findKey(devices, {name: '"10.rotary"'});
-  const rotaryEncoderInput = new InputEvent(rotaryEncoderEvent);
-  const rotaryEncoder      = new InputEvent.Rotary(rotaryEncoderInput);
-
-  rotaryEncoder.on('left', async(/* data */) => {
-//    console.log('Button left');
-
-    glob.left();
-  });
-  rotaryEncoder.on('right', async(/* data */) => {
-//    console.log('Button left');
-
-    glob.right();
-  });
-
-
 
   // Startup main loop
   await loop({oled});
