57431f21c95faf7380ebb1d17e178989a581352c
Stefan Heine <stheine@arcor.de>
2018-08-14 21:34:32 +0200

    switch mute/ shutdown

--- mute.js
+++ mute.js
@@ -7,10 +7,7 @@ const rpio    = require('rpio');
 rpio.init({
   mapping: 'physical',
 });
-rpio.open(37, rpio.OUTPUT, rpio.LOW); // mute
-rpio.sleep(0.5);
-rpio.open(35, rpio.OUTPUT, rpio.LOW); // shutdown
 
-rpio.write(37, rpio.LOW);
-rpio.sleep(0.5);
-rpio.write(35, rpio.LOW);
+rpio.open(35, rpio.OUTPUT, rpio.LOW); // mute
+rpio.sleep(0.1);
+rpio.open(37, rpio.OUTPUT, rpio.LOW); // shutdown
--- unmute.js
+++ unmute.js
@@ -7,7 +7,7 @@ const rpio = require('rpio');
 rpio.init({
   mapping: 'physical',
 });
-rpio.open(37, rpio.OUTPUT, rpio.LOW); // mute
-rpio.open(35, rpio.OUTPUT, rpio.HIGH); // enable
-rpio.sleep(0.75);
-rpio.write(37, rpio.HIGH); // unmute
+rpio.open(35, rpio.OUTPUT, rpio.LOW); // mute
+rpio.open(37, rpio.OUTPUT, rpio.HIGH); // enable
+rpio.sleep(1);
+rpio.write(35, rpio.HIGH); // unmute
