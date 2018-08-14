f06dc3457bccc739d3b16230a186963f8dc0b08e
Stefan Heine <stheine@arcor.de>
2018-08-14 21:21:18 +0200

    menu und streaming und zeit

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 'use strict';
 
 /* eslint-disable no-new */
+/* eslint-disable no-console */
 
 const rpio   = require('rpio');
 const Oled   = require('../sh1106-js/oled.js'); // TODO npm module
 
 const Input  = require('./lib/Input');
+const Logic  = require('./lib/Logic');
 const Loop   = require('./lib/Loop');
 const Menu   = require('./lib/Menu');
 const Render = require('./lib/Render');
@@ -43,18 +45,26 @@ const Stream = require('./lib/Stream');
   // Menu
   const menu   = new Menu({stream});
 
-  // Render
-  const render = new Render({menu, oled});
+  // Logic
+  const logic = new Logic({menu, stream});
+
+  menu.logic = logic;
 
   // Register the handlers for the input events
   new Input({handler: {
-    press: render.press.bind(render),
-    up:    render.up.bind(render),
-    left:  render.left.bind(render),
-    right: render.right.bind(render),
+    press1: logic.press1.bind(logic),
+    up1:    logic.up1.bind(logic),
+    left1:  logic.left1.bind(logic),
+    right1: logic.right1.bind(logic),
+
+    press2: logic.press2.bind(logic),
+    up2:    logic.up2.bind(logic),
+    left2:  logic.left2.bind(logic),
+    right2: logic.right2.bind(logic),
   }});
 
-//  stream.toggle();
+  // Render
+  const render = new Render({logic, menu, oled, stream});
 
   const loop = new Loop({render});
 
--- stream.js
+++ stream.js
@@ -3,33 +3,29 @@
 'use strict';
 
 const request = require('request');
+
 const Speaker = require('speaker');
 const lame    = require('lame');
-const rpio    = require('rpio');
-
-const speaker = new Speaker({
-  channels: 2,
-  bitDepth: 16,
-  sampleRate: 44100,
-  mode: lame.STEREO,
-});
+const Volume  = require('pcm-volume');
 
 const decoder = new lame.Decoder();
-
-rpio.init({
-  gpiomem: false,
-  mapping: 'physical',
+const speaker = new Speaker({
+  channels:   2,
+  bitDepth:   16,
+  mode:       lame.STEREO,
+  device:     'hw:1,0',
 });
-// rpio.i2cBegin();
-rpio.open(35, rpio.OUTPUT, rpio.LOW); // shutdown
-rpio.open(37, rpio.OUTPUT, rpio.LOW); // mute
 
-rpio.write(35, rpio.HIGH);
-rpio.sleep(1);
-rpio.write(37, rpio.HIGH);
+// volume
+const volume = new Volume();
 
-request
-  .get('http://burnfm.radionetz.de:8000/burn-fm.mp3')
-//  .get('http://st01.dlf.de/dlf/01/128/mp3/stream.mp3')
-  .pipe(decoder)
-  .pipe(speaker);
+volume.setVolume(0.15);
+
+const req = request
+// .get('http://burnfm.radionetz.de:8000/burn-fm.mp3');
+.get('http://st01.dlf.de/dlf/01/128/mp3/stream.mp3');
+
+req
+.pipe(decoder)
+.pipe(volume)
+.pipe(speaker);
